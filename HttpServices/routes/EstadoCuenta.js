b98b1c8e7be78405458062a4c6d14a701e43f148
cris666panic <cris93_tian@hotmail.com>
2017-10-02 12:03:21 -0500

    correccion agregar descuentos por local

--- HttpServices/routes/EstadoCuenta.js
+++ HttpServices/routes/EstadoCuenta.js
@@ -359,6 +359,29 @@ router.post('/EstadoCuentaDocente', function (req, res, next)  {
 
 
 
+    parametros.find(
+
+
+
+        {
+            "descripcion": "cierreMes"
+
+        },function (err, datos) {
+            if (err) { return next(err) }
+
+            estado_mes = datos[0].estado;
+
+            console.log(estado_mes);
+
+
+
+
+
+
+            if (estado_mes=="cerrado"){
+
+
+
     var hoy = new Date();
     var dd = hoy.getDate();
     var mm = hoy.getMonth()+1; //hoy es 0!
@@ -431,7 +454,106 @@ router.post('/EstadoCuentaDocente', function (req, res, next)  {
     )
 
 
-});
+            }else {
+
+
+                //si pasa al 15 cambair estado abierto
+
+
+                var hoy = new Date();
+                var dd = hoy.getDate();
+                var mm = hoy.getMonth()+1; //hoy es 0!
+
+                var mes = mm;
+
+
+                var yyyy = hoy.getFullYear();
+                var yyyy1 = hoy.getFullYear();
+
+
+
+
+
+
+
+                var mes1=mm-1;
+
+                if(mes<10) {
+                    mes='0'+mes
+                }
+
+                if(mes1<10) {
+                    mes1='0'+mes1
+
+                }else {
+
+                    if (mes1>12){
+                        var auxf =mes1-12;
+
+                        var anio= yyyy1+1;
+                        mes1='0'+auxf;
+                        yyyy1=anio;
+
+                    }
+
+
+
+                }
+
+
+                if(mm == 1)
+                {
+                    mes1 = 12;
+                    yyyy1 = yyyy - 1;
+
+                }
+
+
+
+                var fechaAnterior =yyyy1+"-"+mes1+"-"+15;
+                var fechaActual = yyyy+"-"+mes+"-"+15;
+
+                var estado_mes={};
+
+                console.log(fechaActual,fechaAnterior);
+
+
+                TipoUsuario.find(
+
+
+
+                    {
+                        "id_docente":  req.body.docente,
+                        "frac_fecha": {
+                            $gte: new Date(fechaActual+"T00:00:00.000Z"),
+                            $lte: new Date(fechaAnterior+"T00:00:00.000Z")
+                        }
+
+                    },function (err, datos) {
+                        if (err) { return next(err) }
+                        res.json(datos);
+                    }
+
+
+                )
+
+
+
+
+            }
+
+
+
+
+
+        }
+
+
+    );
+
+
+
+            });
 
 
 router.get('/AllEstadoCuenta', function (req, res, next)  {
